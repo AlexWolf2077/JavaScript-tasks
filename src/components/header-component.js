9f46e22305f05422c4b750074e0db8742cc6233f
AlexWolf2077 <145503441+AlexWolf2077@users.noreply.github.com>
2023-11-18 14:12:20 +0300

    Список задач

---
+++ src/components/header-component.js
@@ -0,0 +1,26 @@
+import {createElement} from './render';
+function createHeaderComponentTemplate() {
+    return (
+        `<header class="main-header">
+        <div class="nav-bar">
+            <div class="nav-bar-content-block">
+                <h1 class="name-nav-bar">Список задач</h1>
+            </div>
+        </div>
+    </header>`
+      );
+}
+export class HeaderComponent {
+  getTemplate() {
+    return createHeaderComponentTemplate();
+  }
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+    return this.element;
+  }
+  removeElement() {
+    this.element = null;
+  }
+}
---
+++ src/components/main.js
@@ -0,0 +1,4 @@
+import {HeaderComponent} from './header-component.js';
+import {render, RenderPosition} from './render.js';
+const bodyContainer= document.querySelector('.board-app');
+render(new HeaderComponent(), bodyContainer, RenderPosition.BEFOREBEGIN);
